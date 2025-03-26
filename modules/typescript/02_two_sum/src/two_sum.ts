9e354b294e63a09c443e643c52738e1e9614cc0b
Santiago Molina <jasmo2.com@gmail.com>
2025-03-26 18:03:20 -0500

    02_two_sum

--- modules/typescript/02_two_sum/src/two_sum.ts
+++ modules/typescript/02_two_sum/src/two_sum.ts
@@ -1,3 +1,15 @@
 export function twoSum(numbers: number[], target: number): number[] {
-  return [];
+  const numMap: Record<number, number> = {}
+
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i]
+
+    if (complement in numMap) {
+      return [numMap[complement], i]
+    }
+
+    numMap[numbers[i]] = i
+  }
+
+  return []
 }
