870505c38f2068c5926ed7b87000204d321f1007
Santiago Molina <jasmo2.com@gmail.com>
2025-03-26 18:10:34 -0500

    03_retry_promise

--- modules/typescript/03_retry_promise/src/retry_promise.ts
+++ modules/typescript/03_retry_promise/src/retry_promise.ts
@@ -1,9 +1,25 @@
 type RetryPromiseOptions = {
-  retries: number;
-  shouldRetry?: (error: unknown) => boolean;
-};
+  retries: number
+  shouldRetry?: (error: unknown) => boolean
+}
 
 export async function retryPromise<T>(
   callback: () => Promise<T>,
-  options: RetryPromiseOptions
-) {}
+  options: RetryPromiseOptions = { retries: 0 }
+): Promise<T> {
+  const defaultShouldRetry = () => true
+  const shouldRetry = options.shouldRetry ?? defaultShouldRetry
+
+  try {
+    return await callback()
+  } catch (error) {
+    if (options.retries > 0 && shouldRetry(error)) {
+      return retryPromise(callback, {
+        ...options,
+        retries: options.retries - 1,
+        shouldRetry,
+      })
+    }
+    throw error
+  }
+}
