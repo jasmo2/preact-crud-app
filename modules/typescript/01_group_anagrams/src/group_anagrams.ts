14ab063235fec3377713321f7425528f62771e72
Santiago Molina <jasmo2.com@gmail.com>
2025-03-26 17:46:31 -0500

    01_group_anagrams

--- modules/typescript/01_group_anagrams/src/group_anagrams.ts
+++ modules/typescript/01_group_anagrams/src/group_anagrams.ts
@@ -1,3 +1,14 @@
 export function groupAnagrams(input: string[]): string[][] {
-  return [];
+  const anagramMap: Record<string, string[]> = {}
+
+  for (const word of input) {
+    const sortedWord = word.split("").sort().join("")
+
+    const group = anagramMap[sortedWord] || []
+    group.push(word)
+    anagramMap[sortedWord] = group
+  }
+
+  const values = Object.values(anagramMap)
+  return Array.from(values)
 }
