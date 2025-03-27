fb36d3c3fd0374f53ce161b717f943c508e0b78f
Santiago Molina <jasmo2.com@gmail.com>
2025-03-27 11:16:07 -0500

    testing config

--- modules/typescript/04-preact-crud-app/vite.config.ts
+++ modules/typescript/04-preact-crud-app/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite"
+import { defineConfig } from "vitest/config"
 import preact from "@preact/preset-vite"
+import { configDefaults } from "vitest/config"
 
 export default defineConfig({
+  //@ts-ignore
   plugins: [preact()],
   test: {
+    ...configDefaults,
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./src/test/setup.ts"],
+    setupFiles: [".src/test/setup.ts"],
   },
 })
